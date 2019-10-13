cc160683a2665a9d8fd4520e709a8d8155bee1ec
robinfriedli <robinfriedli@icloud.com>
2019-10-13 16:51:46 +0200

    rework ISnowflakeMap to improve performance and thread safety
     - never iterate over the entry set but access the id map of the
       ISnowflakeEntrySet directly

--- src/main/java/net/robinfriedli/botify/util/ISnowflakeMap.java
+++ src/main/java/net/robinfriedli/botify/util/ISnowflakeMap.java
@@ -1,28 +1,29 @@
 package net.robinfriedli.botify.util;
 
 import java.util.AbstractMap;
+import java.util.AbstractSet;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.Set;
 
-import javax.annotation.Nullable;
-
 import net.dv8tion.jda.api.entities.ISnowflake;
+import net.robinfriedli.botify.discord.GuildContext;
 import org.jetbrains.annotations.NotNull;
 
 /**
- * A simple AbstractMap implementation that maps a discord snowflake (e.g user) to any value of type V and uses the
- * snowflake's id instead of the {@link Object#equals(Object)} method
+ * An AbstractMap implementation that maps a discord snowflake (e.g user) to any value of type V and uses the
+ * snowflake's id instead of the {@link Object#equals(Object)} method. This map is mainly used for caching of, for
+ * example, {@link GuildContext} and thus can experience many concurrent operations under high traffic.
  */
 public class ISnowflakeMap<V> extends AbstractMap<ISnowflake, V> {
 
-    private final Set<Entry<ISnowflake, V>> entrySet = new ISnowflakeEntrySet<>();
+    private final ISnowflakeEntrySet<V> entrySet = new ISnowflakeEntrySet<>();
 
     @Override
     public V get(Object key) {
         if (key instanceof ISnowflake) {
-            Entry<ISnowflake, V> entry = getEntry((ISnowflake) key);
-            if (entry != null) {
-                return entry.getValue();
-            }
+            return entrySet.get((ISnowflake) key);
         }
 
         return null;
@@ -34,27 +35,13 @@ public class ISnowflakeMap<V> extends AbstractMap<ISnowflake, V> {
             throw new UnsupportedOperationException("Null keys not supported");
         }
 
-        Entry<ISnowflake, V> entry = getEntry(iSnowflake);
-        if (entry != null) {
-            V oldValue = entry.getValue();
-            entry.setValue(value);
-
-            return oldValue;
-        } else {
-            entrySet.add(new SimpleEntry<>(iSnowflake, value));
-        }
-
-        return null;
+        return entrySet.addOrUpdateEntry(iSnowflake, value);
     }
 
     @Override
     public V remove(Object key) {
         if (key instanceof ISnowflake) {
-            Entry<ISnowflake, V> entry = getEntry((ISnowflake) key);
-            if (entry != null) {
-                entrySet.remove(entry);
-                return entry.getValue();
-            }
+            return entrySet.removeEntryFor((ISnowflake) key);
         }
 
         return null;
@@ -63,7 +50,7 @@ public class ISnowflakeMap<V> extends AbstractMap<ISnowflake, V> {
     @Override
     public boolean containsKey(Object key) {
         if (key instanceof ISnowflake) {
-            return getEntry((ISnowflake) key) != null;
+            return entrySet.containsEntryFor((ISnowflake) key);
         }
 
         return false;
@@ -75,15 +62,97 @@ public class ISnowflakeMap<V> extends AbstractMap<ISnowflake, V> {
         return entrySet;
     }
 
-    @Nullable
-    private Entry<ISnowflake, V> getEntry(ISnowflake iSnowflake) {
-        for (Entry<ISnowflake, V> entry : entrySet) {
-            if (entry.getKey().getId().equals(iSnowflake.getId())) {
-                return entry;
-            }
+    private static class ISnowflakeEntrySet<V> extends AbstractSet<Entry<ISnowflake, V>> {
+
+        private final Map<Long, Map.Entry<ISnowflake, V>> mappedValues = new HashMap<>();
+
+        @NotNull
+        @Override
+        public Iterator<Entry<ISnowflake, V>> iterator() {
+            return mappedValues.values().iterator();
         }
 
-        return null;
+        @Override
+        public int size() {
+            return mappedValues.size();
+        }
+
+        @Override
+        public boolean add(Map.Entry<ISnowflake, V> entry) {
+            long id = entry.getKey().getIdLong();
+            return mappedValues.put(id, entry) == null;
+        }
+
+        @Override
+        public boolean contains(Object o) {
+            if (o instanceof Map.Entry) {
+                Object key = ((Map.Entry) o).getKey();
+                if (key instanceof ISnowflake) {
+                    return mappedValues.containsKey(((ISnowflake) key).getIdLong());
+                }
+            }
+
+            return false;
+        }
+
+        @Override
+        public boolean remove(Object o) {
+            if (o instanceof Map.Entry) {
+                Object key = ((Map.Entry) o).getKey();
+                if (key instanceof ISnowflake) {
+                    Map.Entry<ISnowflake, V> removedEntry = mappedValues.remove(((ISnowflake) key).getId());
+                    return removedEntry != null;
+                }
+            }
+
+            return false;
+        }
+
+        /**
+         * @return the value of the entry mapped to the id of the provided {@link ISnowflake} directly.
+         */
+        private V get(ISnowflake iSnowflake) {
+            Entry<ISnowflake, V> iSnowflakeEntry = mappedValues.get(iSnowflake.getIdLong());
+            return iSnowflakeEntry != null ? iSnowflakeEntry.getValue() : null;
+        }
+
+        private boolean containsEntryFor(ISnowflake iSnowflake) {
+            return mappedValues.containsKey(iSnowflake.getIdLong());
+        }
+
+        /**
+         * Add or update the existing entry for the provided snowflake directly without having to iterate over the entry
+         * map of the surrounding ISnowflakeMap to find the entry by querying the mapped entry directly.
+         *
+         * @param iSnowflake the snowflake to add a new entry for or update
+         * @param value      the new value to assign to the new entry or update the existing one with
+         * @return the previous value or null
+         */
+        private V addOrUpdateEntry(ISnowflake iSnowflake, V value) {
+            long id = iSnowflake.getIdLong();
+            Entry<ISnowflake, V> existingEntry = mappedValues.get(id);
+            V oldVal = null;
+            if (existingEntry != null) {
+                oldVal = existingEntry.getValue();
+                existingEntry.setValue(value);
+            } else {
+                mappedValues.put(id, new SimpleEntry<>(iSnowflake, value));
+            }
+
+            return oldVal;
+        }
+
+        /**
+         * Remove the entry mapped to the id of this snowflake directly instead of first having to search the entry
+         *
+         * @param iSnowflake the {@link ISnowflake} to remove
+         * @return the value of entry that was removed or null
+         */
+        private V removeEntryFor(ISnowflake iSnowflake) {
+            Entry<ISnowflake, V> removedEntry = mappedValues.remove(iSnowflake.getIdLong());
+            return removedEntry != null ? removedEntry.getValue() : null;
+        }
+
     }
 
 }
