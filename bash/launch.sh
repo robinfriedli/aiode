291bfb6b18d2b92cb20734ccd1227a077b16aebb
robinfriedli <robinfriedli@icloud.com>
2021-01-12 22:25:46 +0100

    use botify.preferences.maxHeapSize property in launch script

--- bash/launch.sh
+++ bash/launch.sh
@@ -1,1 +1,7 @@
-java -Xmx1024m -jar build/libs/botify-1.0-SNAPSHOT.jar
+heap_size=$(grep -w "botify.preferences.maxHeapSize" src/main/resources/application.properties|cut -d'=' -f2)
+if [ -z "$heap_size" ]
+then
+  java -jar build/libs/botify-1.0-SNAPSHOT.jar
+else
+  java -Xmx"$heap_size" -jar build/libs/botify-1.0-SNAPSHOT.jar
+fi
