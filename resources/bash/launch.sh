b03878b6ff3a013cb197e11cc261f9065bdaa181
robinfriedli <robinfriedli@icloud.com>
2021-01-13 22:33:56 +0100

    adjust launch script
     - use sun.net.httpserver args as VM options instead of cli args

--- resources/bash/launch.sh
+++ resources/bash/launch.sh
@@ -1,7 +1,7 @@
 heap_size=$(grep -w "HEAP_SIZE" resources/settings.properties|cut -d'=' -f2)
 if [ -z "$heap_size" ]
 then
-  java -jar target/botify-1.0-SNAPSHOT.jar -Dsun.net.httpserver.maxReqTime=30 -Dsun.net.httpserver.maxRspTime=30
+  java -Dsun.net.httpserver.maxReqTime=30 -Dsun.net.httpserver.maxRspTime=30 -jar target/botify-1.0-SNAPSHOT.jar
 else
-  java -Xmx"$heap_size" -jar target/botify-1.0-SNAPSHOT.jar -Dsun.net.httpserver.maxReqTime=30 -Dsun.net.httpserver.maxRspTime=30
+  java -Xmx"$heap_size" -Dsun.net.httpserver.maxReqTime=30 -Dsun.net.httpserver.maxRspTime=30 -jar target/botify-1.0-SNAPSHOT.jar
 fi
