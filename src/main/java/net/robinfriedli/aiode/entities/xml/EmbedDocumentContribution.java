367196bfa568385741b82b1dfdd6c0b6e1965715
robinfriedli <robinfriedli@icloud.com>
2021-11-16 14:22:13 +0100

    expand EmbedDocumentContribution to use scripts as display predicate
     - adjust Scripting field of getting-started document to only be
       displayed if aiode.preferences.enable_scripting is true

--- src/main/java/net/robinfriedli/aiode/entities/xml/EmbedDocumentContribution.java
+++ src/main/java/net/robinfriedli/aiode/entities/xml/EmbedDocumentContribution.java
@@ -2,7 +2,9 @@ package net.robinfriedli.aiode.entities.xml;
 
 import javax.annotation.Nullable;
 
+import groovy.lang.GroovyShell;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.robinfriedli.aiode.Aiode;
 import net.robinfriedli.jxp.api.AbstractXmlElement;
 import net.robinfriedli.jxp.api.XmlElement;
 import net.robinfriedli.jxp.collections.NodeList;
@@ -10,6 +12,8 @@ import net.robinfriedli.jxp.persist.Context;
 import org.w3c.dom.Element;
 
 public class EmbedDocumentContribution extends AbstractXmlElement {
+
+    private final GroovyShell groovyShell = new GroovyShell();
 
     // invoked by JXP
     @SuppressWarnings("unused")
@@ -28,6 +32,10 @@ public class EmbedDocumentContribution extends AbstractXmlElement {
     }
 
     public EmbedBuilder buildEmbed() {
+        Aiode aiode = Aiode.get();
+        groovyShell.setProperty("aiode", aiode);
+        groovyShell.setProperty("properties", aiode.getSpringPropertiesConfig());
+
         String title = getAttribute("title").getValue();
         String description = getAttribute("description").getValue();
 
@@ -36,6 +44,18 @@ public class EmbedDocumentContribution extends AbstractXmlElement {
         embedBuilder.setDescription(description);
 
         for (XmlElement field : getSubElements()) {
+            if (field.hasAttribute("displayPredicate")) {
+                Object displayPredicateResult = groovyShell.evaluate(field.getAttribute("displayPredicate").getValue());
+
+                if (!(displayPredicateResult instanceof Boolean)) {
+                    throw new IllegalStateException("displayPredicate did not return a boolean");
+                }
+
+                if (!((boolean) displayPredicateResult)) {
+                    continue;
+                }
+            }
+
             String fieldTitle = field.getAttribute("title").getValue();
             boolean inline = field.getAttribute("inline").getBool();
             embedBuilder.addField(fieldTitle, field.getTextContent(), inline);
